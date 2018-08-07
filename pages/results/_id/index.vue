b2f936f6dc31317ce2aac273988d3e8e4601c21c
Dendi Handian <handian.dendi@gmail.com>
2018-08-07 08:23:40 +0700

    the search form

---
+++ pages/results/_id/index.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div>
+    <h1>Hello from result route {{ $route.params.id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+h1 {
+  padding: 20px;
+}
+</style>
